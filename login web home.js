709b89eca441ed8456449a915c26c532fe429e65
Jaya-29 <145093421+Jaya-29@users.noreply.github.com>
2024-03-29 09:31:52 +0530

    commit

---
+++ login web home.js
@@ -0,0 +1,32 @@
+// let menu = document.querySelector('#menu-bar'); 
+// let navbar = document.querySelector('.navbar'); 
+
+// window.onscroll = () => {
+//     menu.classList.remove('fa-times')
+//     navbar.classList.remove('active')
+// };
+
+// menu.addEventListener('click', () => {
+//     menu.classList.toggle('fa-times')
+//     navbar.classList.toggle('active')
+// });
+
+
+// formBtn.addEventListener('click', () => {
+//     loginForm.classList.add('active')
+// });
+
+// formClose.addEventListener('click', () => {
+//     loginForm.classList.remove('active')
+// });
+
+
+function myFunctin(){
+    var x = document.getElementById("mynavbar").style.width = "250px"
+
+    if (x.style.display === "block") {
+        x.style.display = "none";
+      } else {
+        x.style.display = "block";
+      }
+    }
